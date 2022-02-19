626ee5a7f13a7c610f58cb335a2aef352de4547e
keithpham <keithpham>
2022-02-19 12:12:57 +0700

    ! add nlohmann json

--- source/windowmanager.cpp
+++ source/windowmanager.cpp
@@ -1,5 +1,6 @@
 #include "windowmanager.h"
 #include "spdlog/spdlog.h"
+#include "nlohmann/json.hpp"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 WindowManager* WindowManager::s_instance = nullptr;
@@ -18,7 +19,13 @@ WindowManager::WindowManager()
 
 int WindowManager::addWindow(IWindow* window)
 {
-    m_listWindows.push_back(window); 
+    m_listWindows.push_back(window);
+
+    // parse explicitly
+    auto j3 = nlohmann::json::parse(R"({"happy": true, "pi": 3.141})");
+    std::string s = j3.dump();
+
+    spdlog::info(s);
 
     return m_listWindows.size() - 1;
 }
